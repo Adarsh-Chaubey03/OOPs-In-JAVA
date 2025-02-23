77ae7136129fe003eb11c1b1cc59a89fd2e73aaf
Adarsh-Chaubey03 <adarsh03chaubey@gmail.com>
2025-02-23 16:27:11 +0530

    Create MethodOverriding.java

---
+++ src/adarsh/H_Properties_Of_OOPs/A_Inheritance/Basics/MethodOverriding.java
@@ -0,0 +1,31 @@
+package adarsh.H_Properties_Of_OOPs.A_Inheritance.Basics;
+
+class Animal {
+    void sound() {
+        System.out.println("Animal makes sound");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+        super.sound();
+    }
+}
+
+
+public class MethodOverriding {
+    public static void main(String[] args) {
+
+        Animal a = new Animal();
+        Dog d = new Dog();
+
+        a.sound(); // Calls Animal's sound method  ---> Animal makes sound
+        d.sound();
+        /* Calls Dog's sound method, then Animal's sound method via super
+        Dog barks
+        Animal makes sound
+         */
+    }
+}
